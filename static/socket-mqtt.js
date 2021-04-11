0383df87df8c53ac99a67a8e36bb4829fdea3fae
lxy764139720 <764139720@qq.com>
2021-04-11 11:25:55 +0800

    fix: 地图

--- static/socket-mqtt.js
+++ static/socket-mqtt.js
@@ -1,4 +1,7 @@
 var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+var img = new Image();
+var last_x = 546;
+var last_y = 20;
 
 socket.on('connect', function () {
     socket.emit('connect_message', {data: 'connected!'});
@@ -11,17 +14,22 @@ socket.on('odom', function (msg) {
     console.log(msg);
     // ((?<=<x:).+)(.+(?=,))
     // ((?<=,).+)(.+(?=>))
-    var x = parseFloat(msg.payload.match(/((?<=<x:).+)(.+(?=,))/)[0]);
-    var y = parseFloat(msg.payload.match(/((?<=,y:).+)(.+(?=>))/)[0]);
+    var x = parseFloat(msg.payload.match(/((?<=<x).+)(.+(?=,))/));
+    var y = parseFloat(msg.payload.match(/((?<=, y).+)(.+(?=>))/));
 
     var canvas = document.getElementById('map');
     if (!canvas.getContext) return;
     var ctx = canvas.getContext("2d");
-    var img = new Image();
+
+    x_img = 78.83 * x - 31.24;
+    console.log("x:" + x.toString());
+    y_img = -61.59 * y - 0.324;
+    console.log("y:" + y.toString());
     img.onload = function () {
-        console.log("x:" + x.toString());
-        console.log("y:" + y.toString());
-        ctx.drawImage(img, x, y, 20, 20);
+        ctx.clearRect(last_x,last_y,20,20);
+        ctx.drawImage(img, x_img, y_img, 20, 20);
+        last_x = x_img;
+        last_y = y_img;
     }
     img.src = "../static/images/location.png"; // 设置图片源地址
 });
@@ -47,7 +55,7 @@ function unsubscribe() {
     socket.emit('unsubscribe', {topic: 'odom', message: ''})
 }
 
-function toast(){
+function toast() {
     console.log("click toast");
     socket.emit('toast');
 }
